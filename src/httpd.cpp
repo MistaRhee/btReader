9c06b687af7cc6c7074bd57f57a1307688357647
Justin Huang <jhuangmystery@gmail.com>
2015-02-02 22:14:32 +1100

    Fixed up httpd Also learnt about that BT has an API >.< Soooooooooo I guess I'll be working with the API instead of doing dumbass stripping solution......

--- src/httpd.cpp
+++ src/httpd.cpp
@@ -30,3 +30,30 @@ void cHttpd::download(const std::string url, const std::string fileName){
     }
     fclose(fin);
 }
+
+std::string qDownload(const std::string urli, int nameLen){
+    std::string fileName = generateRandomName(nameLen);
+    FILE* fin;
+    fin = fopen(fileName.c_str(), "w+");
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
+    //curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING, "deflate");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, fin);
+    CURLcode res = curl_easy_perform(curl);
+    if (res != CURLE_OK) {
+        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    }
+    fclose(fin);
+}
+
+std::string generateRandomName(int length){
+    srand(time(NULL));
+    const char aCharacters[] = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    std::string rVal;
+    for(int i = 0, j = strlen(aCharacters); i < length; i++){
+        rVal += aCharacters[rand()%j];
+    }
+    return rVal;
+}
