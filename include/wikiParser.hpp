b7d3c5eb093848421112ab02662e1aa88e323e64
NoOne2246 <christopher.ho@hotmail.com>
2015-08-24 11:23:50 +1000

    forgot to add parser header

---
+++ include/wikiParser.hpp
@@ -0,0 +1,46 @@
+# ifndef WKIPARSER_HPP
+#define WIKIPARSER_HPP
+
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <map>
+#include <thread>
+#include <utility>
+#include <ctime>
+#include <SDL2/SDL.h>
+#include <string>
+#include <cstring>
+#include <exception>
+
+
+typedef struct _links_info{
+    bool link;
+    bool internal;
+    bool available;
+    std::string link;
+    std::string text;
+}links_info;
+
+typedef struct Wikitext{
+    char type;
+    int level;
+    std::string text;
+};
+
+
+class cWikiParser{
+    public:
+        void open(const std::string, const std::string);       //send data returned from api query toi be opened. 
+        void close();                       //cklears registry of data.
+        int nquery(const std::string);      //get count of a ceratin element
+        std::string query(const std::string);                   //query for the data in the withint the element with their text = string.
+        std::string query(const std::string, const int);//query the ith element under the text = string.
+    private:
+        std::string cWikiParser::textMarkup(std::string);   //cleans texts of links and images replacing with UID as well as marks up bold and italic fonts.
+        struct cWikiParser::linksChecker(std::string);      //checks if links are available and other things to do with images and links.
+
+        //storage of information for later use
+        std::vector<WikiText> database;
+};
