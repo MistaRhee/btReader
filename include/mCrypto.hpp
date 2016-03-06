6dc2f44523ed4b50ed841bb5d90b784a4204b3cd
MistaRhee <jhuangmystery@gmail.com>
2016-03-07 09:43:25 +1100

    Fixed up crypto library *GEH*

--- include/mCrypto.hpp
+++ include/mCrypto.hpp
@@ -3,9 +3,9 @@
 
 class cCrypt{
     public:
-        char* crypth(const char* string); //Hashes a string
-        char* crypts(const char* string, const char* key); //Encrypts a string
-        char* decrypts(const char* string, const char* key); //Decrypts a string
+        static char* crypth(const char* string); //Hashes a string
+        static char* crypts(const char* string, const char* key); //Encrypts a string
+        static char* decrypts(const char* string, const char* key); //Decrypts a string
 };
 
 #endif
